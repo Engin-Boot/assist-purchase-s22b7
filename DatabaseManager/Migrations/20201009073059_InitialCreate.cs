5451606c479ea6667361ab150b99418ab16431a6
AdarshSrivatsa98 <adarsh.98srivatsa@gmail.com>
2020-10-09 13:07:17 +0530

    Database example init

---
+++ DatabaseManager/Migrations/20201009073059_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    DisplaySize = table.Column<int>(nullable: false),
+                    DisplayType = table.Column<string>(nullable: true),
+                    Weight = table.Column<double>(nullable: false),
+                    Battery = table.Column<bool>(nullable: false),
+                    TouchScreen = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ DatabaseManager/Model.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseManager
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+   
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=products.db");
+    }
+
+    public class Product
+    {
+        public string Id { get; set; }
+        public int DisplaySize { get; set; }
+        public string DisplayType { get; set; }
+        public double Weight { get; set; }
+        public bool Battery { get; set; }
+        public bool TouchScreen { get; set; }
+    }
+}
+
+
+
+
+
+

dcb97f01ab1a749e8f27c70de21b75926541b273
Zoting <Pallavi.Zoting@philips.com>
2020-10-16 01:00:50 +0530

    API Test

--- AssistPurchaseTest/ApiControllerTest/SalesDataControllerTest.cs
+++ AssistPurchaseTest/ApiControllerTest/SalesDataControllerTest.cs
@@ -1,6 +1,58 @@
-﻿namespace AssistPurchaseTest.ApiControllerTest
+﻿using Xunit;
+using AssistPurchase.Controllers;
+using DatabaseManager;
+using DatabaseContractor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssistPurchaseTest.ApiControllerTest
 {
-    class SalesDataControllerTest
+    public class SalesDataControllerTest
     {
+        private SalesDatabaseHandler _service;
+        SalesDataController _controller;
+
+        public SalesDataControllerTest()
+        {
+            _service = new SalesDatabaseHandler();
+            _controller = new SalesDataController(_service);
+        }
+        // Add test case
+        [Fact]
+        public void Add_InvalidObjectPassed_ReturnsBadRequest()
+        {
+            var nameMissingItem = new Sales
+            {
+               EmailId="tom123@gmail.com",
+               Description="Message",
+            };
+            var badResponse = _controller.Post(nameMissingItem);
+
+            Assert.IsType<BadRequestObjectResult>(badResponse);
+        }
+        [Fact]
+        public void Add_ValidObjectPassed_ReturnsCreatedResponse()
+        {
+
+            var testItem = new Sales()
+            {
+                CustomerName="tom",
+                EmailId="tom123@gmail.com",
+                Description="Message",
+            };
+
+            var createdResponse = _controller.Post(testItem);
+
+            Assert.IsType<CreatedAtActionResult>(createdResponse);
+        }
+
+        // Get Test Cases
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.GetAllInfo();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult);
+        }
     }
 }

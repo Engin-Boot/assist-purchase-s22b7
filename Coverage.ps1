620017a4f422c52daf14928b42f01c3dfff1b1af
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-10-15 22:25:35 +0530

    Update Coverage.ps1

--- Coverage.ps1
+++ Coverage.ps1
@@ -11,7 +11,7 @@ function WriteXmlToScreen ([xml]$xml)
     Write-Output $StringWriter.ToString();
 }
 
-$report = Get-Content -Path WebApiTest\TestResults\*\coverage.cobertura.xml | Out-String
+$report = Get-Content -Path AssistPurchaseTest\TestResults\*\coverage.cobertura.xml | Out-String
 Write-Host "---------------------------------"
 Write-Host "Code Coverage report ..." 
 Write-Host "---------------------------------"
